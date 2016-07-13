8064216897d3d951e792092e702ffd797c21baf3
Juan Francisco <jmaldonado@kvz.cl>
2016-07-13 14:38:10 -0400

    icons

--- src/main/webapp/js/controladores/TeamController.js
+++ src/main/webapp/js/controladores/TeamController.js
@@ -2,8 +2,13 @@
  * @author Juan Francisco ( juan.maldonado.leon@gmail.com )
  * @desc Controlador PerfilEquipoController
  *************************************************************/
-app.controller("TeamController", function($scope, $http)
-{
+app.controller("TeamController", function($scope, $http, $location){
+	
+	$scope.ref = function( link ){
+		$location.url( link );
+	};
+	
+	
 	$scope.teams = [];
 	
 	$scope.teams.push( {oid:1,name:"Chicago Bulls", gender:'MALE', description:'', category:'ADULT', image:'cbulls.png'} );
